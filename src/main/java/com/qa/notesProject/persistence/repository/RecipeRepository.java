976141264a8146351e016fcfa7811278cced97b9
CodewithOsatQA <OTowesho@qa.com>
2020-10-02 14:29:15 +0100

    The repositories have been added

--- src/main/java/com/qa/notesProject/persistence/repository/RecipeRepository.java
+++ src/main/java/com/qa/notesProject/persistence/repository/RecipeRepository.java
@@ -2,9 +2,10 @@ package com.qa.notesProject.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.qa.notesProject.persistence.domain.Ingredients;
+
+import com.qa.notesProject.persistence.domain.Recipe;
 
 @Repository
-public interface IngredientRepository extends JpaRepository<Ingredients, Long>{
-	
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
 }
