cd5b11e6c6777d54ddfd532287a61882ef8441c9
CodewithOsatQA <OTowesho@qa.com>
2020-10-02 12:51:12 +0100

    initialising

---
+++ src/test/java/com/qa/notesProject/NotesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.notesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
